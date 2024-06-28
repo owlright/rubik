75fe147061520157628282482f3975402486e8ad
owlright <71282734+owlright@users.noreply.github.com>
2024-06-28 22:21:22 +0800

    Format

--- triangle.cpp
+++ triangle.cpp
@@ -44,8 +44,8 @@ int main(int, char**)
     /* -------------------------------------------------------------------------- */
     float vertices[] = {
         -0.5f, -0.5f, 0.0f, // left
-        0.5f, -0.5f, 0.0f, // right
-        0.0f, 0.5f, 0.0f // top
+        0.5f, -0.5f, 0.0f,  // right
+        0.0f, 0.5f, 0.0f    // top
     };
 
     unsigned int VBO, VAO;
